8137d3e7c45f4a460c0701c02c42bc33a0653543
Kevin Hu <hu.kevin.w@gmail.com>
2019-05-26 18:21:57 -0400

    able to create post

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -11,4 +11,18 @@ describe 'navigate' do
       expect(page).to have_content(/Posts index/)
     end
   end 
+
+  describe 'creation' do
+    it 'can reach new form' do
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+    end
+    it 'can be created on new form page' do 
+      visit new_post_path 
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "rationale"
+      click_on "Save"
+      expect(page).to have_content("rationale")
+    end
+  end 
 end
