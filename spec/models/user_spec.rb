aa896ac34686904d22cf611ad68a056e780fabf7
Kevin Hu <hu.kevin.w@gmail.com>
2019-05-26 13:57:07 -0400

    Spec test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Creation" do 
+    before do
+      @user = User.create( 
+        email: 'test@test.test',
+        first_name: "john",
+        last_name: "snow",
+        password: "asdfasdf",
+        password_confirmation: "asdfasdf",
+      )
+    end
+    it "can be created" do 
+      expect(@user).to be_valid
+    end
+    it "can not be created w/o first_name, last_name" do 
+      @user.first_name = nil
+      @user.last_name = nil
+      expect(@user).to_not be_valid
+    end
+    
+  end
 end
