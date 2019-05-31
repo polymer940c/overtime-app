a0485ec3f9cbfe54d5763bc8f08aca96175d7fd9
Kevin Hu <hu.kevin.w@gmail.com>
2019-05-30 22:42:47 -0400

    Jquery, bootstrap, gem and install

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,7 +6,7 @@ describe 'navigate' do
       visit posts_path
       expect(page.status_code).to eq(200)
     end
-    it "has a title of posts " do
+    it 'has a title of posts' do
       visit posts_path
       expect(page).to have_content(/Posts index/)
     end
@@ -14,16 +14,34 @@ describe 'navigate' do
 
   describe 'creation' do
     before do 
+      @test_user = User.create( 
+        email: 'test@test.test',
+        first_name: "john",
+        last_name: "snow",
+        password: "asdfasdf",
+        password_confirmation: "asdfasdf",
+      )
+      login_as(@test_user, :scope => :user)
       visit new_post_path
     end
+
     it 'can reach new form' do
       expect(page.status_code).to eq(200)
     end
+
     it 'can be created on new form page' do 
       fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: "rationale"
-      click_on "Save"
-      expect(page).to have_content("rationale")
+      fill_in 'post[rationale]', with: 'rationale'
+      click_on 'Save'
+      expect(page).to have_content('rationale')
+    end
+    
+    it 'will have a user associated it' do 
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'rationale'
+      click_on 'Save'
+
+      expect(Post.last.user.id).to eq(@test_user.id)
     end
   end 
 end
