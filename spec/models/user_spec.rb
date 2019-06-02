94607ec10ca32aee0b46921a639e12a19fe944f2
Kevin Hu <hu.kevin.w@gmail.com>
2019-06-02 11:44:11 -0400

    Custom method full_name for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = User.create( 
+      email: 'test@test.test',
+      first_name: "john",
+      last_name: "snow",
+      password: "asdfasdf",
+      password_confirmation: "asdfasdf",
+    )
+  end
+
   describe "Creation" do 
-    before do
-      @user = User.create( 
-        email: 'test@test.test',
-        first_name: "john",
-        last_name: "snow",
-        password: "asdfasdf",
-        password_confirmation: "asdfasdf",
-      )
-    end
-    it "can be created" do 
+    it "can create user" do 
       expect(@user).to be_valid
     end
     it "can not be created w/o first_name, last_name" do 
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
-    end
-    
+    end  
   end
+
+  describe "Custom methods" do 
+    it "can get user full name" do 
+      expect(@user.full_name).to eq("John Snow")
+    end
+  end
+
 end
