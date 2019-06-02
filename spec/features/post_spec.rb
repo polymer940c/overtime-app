56632d3db2d2fb7e6840a7ad0a08fb311c127900
Kevin Hu <hu.kevin.w@gmail.com>
2019-06-02 11:29:18 -0400

    Refactor post to partials

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -10,16 +10,17 @@ describe 'navigate' do
       password_confirmation: "asdfasdf",
     )
     login_as(@test_user, :scope => :user)
-    visit new_post_path
   end
 
   describe 'index' do
+    before do 
+      visit posts_path
+    end
+
     it 'can be reached successfully' do
-      # visit posts_path
       expect(page.status_code).to eq(200)
     end
     it 'has a title of posts' do
-      # visit posts_path
       expect(page).to have_content(/Posts index/)
     end
     it 'has a list of posts' do 
@@ -35,7 +36,11 @@ describe 'navigate' do
     end
   end 
 
-  describe 'creation' dor
+  describe 'creation' do 
+    before do 
+      visit new_post_path
+    end
+    
     before do 
       login_as(@test_user, :scope => :user)
       visit new_post_path
