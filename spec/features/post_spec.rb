3060da6b697cbc6fbbe7a871f3bb3240d0b42e84
Kevin Hu <hu.kevin.w@gmail.com>
2019-06-02 11:14:58 -0400

    Test for posts index + rpsec

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,26 +1,42 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do 
+    @test_user = User.create( 
+      email: 'test@test.test',
+      first_name: "john",
+      last_name: "snow",
+      password: "asdfasdf",
+      password_confirmation: "asdfasdf",
+    )
+    login_as(@test_user, :scope => :user)
+    visit new_post_path
+  end
+
   describe 'index' do
     it 'can be reached successfully' do
-      visit posts_path
+      # visit posts_path
       expect(page.status_code).to eq(200)
     end
     it 'has a title of posts' do
-      visit posts_path
+      # visit posts_path
       expect(page).to have_content(/Posts index/)
+    end
+    it 'has a list of posts' do 
+      5.times do |n|
+        Post.create!(
+          date: Date.today, 
+          rationale: "Post#{n} rationale content: #{@test_user.first_name}", 
+          user_id: @test_user.id
+        )
+      end
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
     end
   end 
 
-  describe 'creation' do
+  describe 'creation' dor
     before do 
-      @test_user = User.create( 
-        email: 'test@test.test',
-        first_name: "john",
-        last_name: "snow",
-        password: "asdfasdf",
-        password_confirmation: "asdfasdf",
-      )
       login_as(@test_user, :scope => :user)
       visit new_post_path
     end
