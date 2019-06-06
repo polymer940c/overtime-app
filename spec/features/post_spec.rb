98250744f5cebf79124fe63af0b688596f8e82d0
Kevin Hu <hu.kevin.w@gmail.com>
2019-06-06 04:57:26 -0400

    add form style for post

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -82,7 +82,16 @@ describe 'navigate' do
       click_link('new_post_from_nav')
       expect(page.status_code).to eq(200)
     end
-    
+  end
+
+  describe 'delete' do 
+    it 'can be deleted' do
+      @post = FactoryBot.create(:post)
+
+      visit posts_path 
+      click_link("delete_post_#{@post.id}_from_index")
+      expect(page.status_code).to eq(200)
+    end
   end
 
 end
