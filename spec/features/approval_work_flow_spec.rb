afe5e68582ddf428c78ccc200244f3bd81b806bf
Kevin Hu <hu.kevin.w@gmail.com>
2019-06-09 19:48:45 -0400

    Permission for updating status

--- spec/features/approval_work_flow_spec.rb
+++ spec/features/approval_work_flow_spec.rb
@@ -1,18 +1,37 @@
 require 'rails_helper'
-
-describe 'navigate' do
-  before do
-    @admin_user = FactoryBot.create(:admin_user)
-    login_as(@admin_user, :scope => :user)
+describe "approval work flow" do 
+  before do 
+    @post = FactoryBot.create(:post)
   end
 
-  describe 'edit' do 
-    it 'can edit post status by form' do 
-      @post = FactoryBot.create(:post)
-      visit edit_post_path(@post)
-      choose('post_status_approved')
-      click_on('Save')
-      expect(@post.reload.status).to eq('approved')
+  describe "as an admin" do 
+    before do
+      @admin_user = FactoryBot.create(:admin_user)
+      login_as(@admin_user, :scope => :user)
+    end
+    describe 'edit' do 
+      it 'can be edited by admin' do 
+        visit edit_post_path(@post)
+        choose('post_status_approved')
+        click_on('Save')
+        expect(@post.reload.status).to eq('approved') 
+      end
     end
   end
+  
+  describe "as a regular joe" do 
+    before do
+      regular_joe = FactoryBot.create(:user)
+      login_as(regular_joe, :scope => :user)
+    end
+    describe 'edit' do
+      it 'can not be edited by regular joe' do 
+        visit edit_post_path(@post)
+        expect(page).to_not have_content('Approved')
+      end
+    end
+  end
+
+
+
 end
